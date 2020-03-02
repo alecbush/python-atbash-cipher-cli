411944c61f07afdcdecd0e5e75854fb388e2c051
Alec Bush <alexander.joseph.bush@gmail.com>
2020-03-02 09:15:34 -0500

    modify dev version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='atbash',
-    version='1.0.0',
+    version='0.1.0',
     packages=['atbash'],
     include_package_data=True,
     license='MIT License',
